ed6aa7f6adf9e397a8c29a6f4eb104e9f01c2b88
Bemnet kassa Bekele <bemnetkassa79@gmail.com>
2026-03-26 11:36:55 +0300

    fixed the html wrap haydration error by removing the html and putting the simple layout for the createjob and list job folder

--- app/createJob/layout.tsx
+++ app/createJob/layout.tsx
@@ -1,15 +1,7 @@
 export default function DashboardLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body>
-        {/* Layout UI */}
-        {/* Place children where you want to render a page or nested layout */}
-        <main>{children}</main>
-      </body>
-    </html>
-  )
+  return <main className="min-h-full">{children}</main>;
 }
